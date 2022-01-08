614452a9cdba11f3cb1d12dffe2c9a00a5a3acaa
Maitrish Mukherjee <maitrish03@gmail.com>
2022-01-08 16:19:01 +0530

    first commit

---
+++ minimum.js
@@ -0,0 +1,18 @@
+let num=prompt('Enter how many numbers you wanna find minimum of')
+let output=document.getElementById('output')
+let min
+function minimum(...inputs){
+    for(let j=0;j<num;j++){
+        let elem=prompt(`Enter ${j+1}st number`)
+        inputs.push(elem)
+    }
+    min=inputs[0]
+    for(let i=0;i<num;i++){
+        if(inputs[i]<min){
+            min=inputs[i]
+        }
+    }
+   
+    output.innerHTML=`${min} is the minimum among ${inputs}`
+}
+inputs.minimum()
